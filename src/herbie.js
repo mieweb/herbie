2c935dba43a6cb6c1dcb240d241114984165bd97
Doug Horner <horner@mieweb.com>
2015-04-09 02:32:31 -0400

    Added press, timeout, etc.  non-working.

--- src/herbie.js
+++ src/herbie.js
@@ -43,7 +43,7 @@ function FindDesc(desc) {
 		if (el.length===1) return el;
 //	}
 
-
+	return [];
 }
 
 // This function takes a human readable potientially multi-lined script and turns it into a structured array.
@@ -53,7 +53,7 @@ function ParseScript(script) {
 
 	for (i = 0; i < lines.length; i++) {  // Go thru each line.
 
-		var cmd = { line: i, code: [], src: lines[i] };  // setup cmd structure
+		var cmd = { line: i, code: [], src: lines[i], timeout: 5000 };  // setup cmd structure
 
 		var stmt = lines[i].match(/\w+|'[^']+'|"[^"]+"|\{\{(.*?)\}\}|\*|:/g);   // break the line into words, "quoted" or 'quoted' tokens, and {{tags}}
 		if (stmt)
@@ -77,6 +77,7 @@ function ParseScript(script) {
 							case 'wait':
 							case 'switch':
 							case 'navigate':
+							case 'press':
 
 							// nouns
 							case 'button':
@@ -110,7 +111,7 @@ function ParseScript(script) {
 }
 
 function ExecuteScript( /* cmdtree, options, callback */) {
-  var cmdtree = arguments[0], options = { line: 0, delay: 100, cmdtree:cmdtree } , callback, tag;
+  var cmdtree = arguments[0], options = { line: 0, delay: 100, cmdtree:cmdtree } , callback, tag = [];
 
 	if (arguments.length == 2) { // only two arguments supplied
 		if (Object.prototype.toString.call(arguments[1]) == "[object Function]") {
@@ -124,10 +125,7 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 	}
 
 	var i=options.line;
-
-	if (i<cmdtree.length) {
-		if (callback) callback(false, options);
-	} else {
+	if (i>=cmdtree.length) {
 		if (callback) callback(true, options);
 		return;
 	}
@@ -139,17 +137,38 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 			tagname = tagname.slice(1,-1);
 
 		tag = FindDesc( tagname );
-		if (!tag) if (callback) callback(false, options, "Cannot find tag named: '" + tagname + "'");
+		if (!tag.length) {
+			if (cmdtree[i].timeout>0) {
+				cmdtree[i].timeout -= options.delay;
+				return setTimeout(function () { ExecuteScript(cmdtree,options,callback); }, options.delay);
+			} else {
+				if (callback) callback(false, options, "Cannot find tag named: '" + tagname + "'");
+			}
+		}
 	}
 
-	switch (cmdtree[i].code[0]) {
-		case 'type':
+	if (callback) callback(false, options);
 
+	switch (cmdtree[i].code[0]) {
+		case 'press':
 			var seq = cmdtree[i].code[1];
 			if (seq.charAt(0)==='"'||seq.charAt(0)==="'")
 				seq = seq.slice(1,-1);
 
-			if (tag) tag.fadeOut(100)
+			if (!tag.length) tag = $( document.activeElement );
+			tag.simulate("key-combo", {combo: seq });
+			tag.next().focus();
+			return setTimeout(function () {
+					options.line++; // ok, setting the options to the next line here.
+					ExecuteScript(cmdtree,options,callback);
+				}, options.delay);
+
+		case 'type':
+			var seq = cmdtree[i].code[1];
+			if (seq.charAt(0)==='"'||seq.charAt(0)==="'")
+				seq = seq.slice(1,-1);
+
+			if (tag.length) tag.fadeOut(100)
 						.fadeIn(100)
 						.fadeOut(100)
 						.fadeIn(100)
@@ -161,7 +180,7 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 							});
 			return;
 		case 'click':
-			if (tag) tag.fadeOut(100)
+			if (tag.length) tag.fadeOut(100)
 						.fadeIn(100)
 						.fadeOut(100)
 						.fadeIn(100)
