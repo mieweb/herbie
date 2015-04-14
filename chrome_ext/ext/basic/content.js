48542cbf85819fb242657fcef7d84ed86845b75e
Doug Horner <horner@mieweb.com>
2015-04-14 01:57:12 -0400

    Heavy refactoring of code to allow the background page more control and herbie browser independant.

--- chrome_ext/ext/basic/content.js
+++ chrome_ext/ext/basic/content.js
@@ -1,48 +1,36 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-//document.body.innerHTML = "<h1>hi there</h1>";
-var t = $(".herbie_div");
-if (t.length) {
-  t.show();
-} else {
-  $.when(
-    $.get(chrome.extension.getURL("dist/jquery.simulate.js"), eval),
-    $.get(chrome.extension.getURL("dist/bililiteRange.js"), eval),
-    $.get(chrome.extension.getURL("dist/jquery.simulate.ext.js"), eval),
-    $.get(chrome.extension.getURL("dist/jquery.simulate.drag-n-drop.js"), eval),
-    $.get(chrome.extension.getURL("dist/jquery.simulate.key-sequence.js"), eval),
-    $.get(chrome.extension.getURL("dist/jquery.simulate.key-combo.js"), eval),
-    $.get(chrome.extension.getURL("herbie/inspector.js"), eval),
-    $.Deferred( function( deferred ) {
-      $('body').append("<div id='herbie'></div>");
-      $("#herbie").load(chrome.extension.getURL("herbie/herbie.html"), function(){
-        $(this).contents().unwrap();  
-        $('#herbie_logo').attr("src",chrome.extension.getURL("herbie48.png"));
-        $( deferred.resolve );
-      });
-    }),
-    $.Deferred( function( deferred ) { $( deferred.resolve ); } )
-  ).done( function() {
-    $.get(chrome.extension.getURL("herbie/herbie.js"),eval);
-    $("#herbie_script").text(
-  "Type 'hi' in to 'Address' input.\n\
-  Fill out the following fields: \n\
-  * Last Name: Horner\n\
-  * Phone: 260-459-6270\n\
-  Click on the 'Save' button. \n\
-  Click 'OK'.\n\
-  Type 'finally' in 'Slow Input'\n\
-  Click 'html body div.sample:nth-child(14) button:nth-child(24)'\n\
-  Click on the 'going away' link.");
-  });
+// content.js is for holding chrome specific code to interface to herbie that runs in the page's content.
+// This code is the interface between the background page and the herbie code that is meant to be browser independent.
+
+function HerbieonMessage(msg) {
+  console.log("Herbie Message", msg );
+  chrome.runtime.sendMessage( msg );
 }
 
 chrome.runtime.onMessage.addListener(function(msg, _, sendResponse) {
-  $('#herbie_output').append("Got message from background page: " + msg + "\n");
-  console.log("Got message from background page: " + msg);
-  if (msg == "Stop") {
-    $(".herbie_div").hide();
+  //$('#herbie_output').append("Got message from background page: " + msg + "\n");
+  console.log("onMessage: ",msg);
+
+  try {
+    var cmd;
+    if (typeof msg === 'string') {
+      cmd = msg;
+    } else {
+      cmd = msg.cmd;
+    }
+
+    switch (cmd) {
+      case "Stop":
+        Herbie.Stop();
+        break;
+      case "Show":
+          Herbie.BuildUI(chrome.extension.getURL("/"), msg.script, HerbieonMessage);
+        break;
+      case "Run":
+        Herbie.StartScript( { script: msg.script, line: msg.line } ,HerbieonMessage);
+        break;
+    }
+  } catch (e) {
+    console.log(e.stack);
   }
 });
