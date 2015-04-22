5db10abae4a068586329c91dad4eb54c9cac2574
Jason Swing <jswing@mieweb.com>
2015-04-22 12:09:18 -0400

    code cleanup, enforcing standards and optimizations

--- src/herbie.js
+++ src/herbie.js
@@ -1,3 +1,53 @@
+/*
+NOTES FOR DOUG TO READ
+
+1. console.log() will throw errors in old IE.  This needs fixed.
+
+2. Wrapping an element in jQuery every time is not efficient, especially when jQuery has to search the DOM each time. The rule of thumb is "the DOM IS SLOW".
+
+ex:
+$('#myele').whatever(...);
+$('#myele').whatever(...);
+$('#myele').whatever(...);
+
+Either set a variable or chain your methods.
+
+ex:
+var ele = $('#myele')
+
+ele.whatever(...);
+ele.whatever(...);
+ele.whatever(...);
+
+--or--
+
+$('#myele').whatever(...).whatever(...).whatever(...);
+
+
+3. MIE has standards regarding coding style.  You should learn them. http://sonar.med-web.com:9000/coding_rules#languages=js|s=createdAt|asc=false
+
+Most notably:
+	a. always use curly braces for "if/else/for/while/do" statements: http://sonar.med-web.com:9000/coding_rules#rule_key=javascript%3ACurlyBraces
+	b. single quotes should be used for string literals http://sonar.med-web.com:9000/coding_rules#rule_key=javascript%3ASingleQuote
+	c. sections of code should not be "commented out" http://sonar.med-web.com:9000/coding_rules#rule_key=javascript%3ACommentedCode
+	d. "===" and "!==" should be used instead of "==" and "!="
+
+	
+4. jQuery selector performance: http://24ways.org/2011/your-jquery-now-with-less-suck
+
+jQuery extends native DOM locators/selectors (getElementsByTagName, getElementsByClassName etc) with additional tools (like :contains()) but applying a non-native selector onto a native selector makes the entire selector into a non-native selector and MUCH SLOWER.  Selectors should be separated so the non-native portion only has to search the subset already returned from the native portion.
+
+before:
+$('label:contains(sometext)');
+
+after:
+$('label').filter(':contains(sometext)');
+  ^native         ^non-native
+
+
+5. This file has mixed indention (tabs and spaces on the same line!). I'll be fixing that as well. Please fix whatever editor you're using to be consistent.
+
+*/
 
 (function(window) {
 
@@ -11,57 +61,65 @@ function FindDesc(desc) {
 
 	if (!desc.match(':$')) { // We should first try to find labels ending with a :
 		desc += ':';
-	} else 
+	} else {
 		hadterm = 1;
+	}
 
 	try {
-		el = $('label:contains(' +desc+')');
-		if (el.length) { 
+		el = $('label').filter(':contains(' +desc+')');
+		if (el.length) {
 			el = el.first();
-			return $('#'+el.attr("for"));  // return the element the label is for
+			return $('#'+el.attr('for'));  // return the element the label is for
 		}
 	} catch (ex) {}
 
 		desc = desc.slice(0,-1);  // remove the traling :
 
 	try {
-		el = $('label:contains(' +desc+')');
-		if (el.length) { 
+		el = $('label').filter(':contains(' +desc+')');
+		if (el.length) {
 			el = el.first();
-			return $('#'+el.attr("for"));  // return the element the label is for
+			return $('#'+el.attr('for'));  // return the element the label is for
 		}
 	} catch (ex) {}
 
-		if (hadterm) desc += ':';
+		if (hadterm) {
+			desc += ':';
+		}
 
 	try {
-		el = $('button:contains(' +desc+')');  // look for buttons that contain that text.
-		if (el.length) 
+		el = $('button').filter(':contains(' +desc+')');  // look for buttons that contain that text.
+		if (el.length) {
 			return el.first();
+		}
 	} catch (ex) {}
 
 	try {
-		el = $('a:contains(' +desc+')');  // look for buttons that contain that text.
-		if (el.length) 
+		el = $('a').filter(':contains(' +desc+')');  // look for buttons that contain that text.
+		if (el.length) {
 			return el.first();
+		}
 	} catch (ex) {}
 
-	//	if (desc.match('^#')) {
 	// as a last ditch effort see if it's a path
 	try {
 		el = $( desc );
 	} catch(e) {
 		el = [];
 	}
-	if (el.length===1) return el;
-	//	}
+	if (el.length===1) {
+		return el;
+	}
 
 	return [];
 }
 
 // This function takes a human readable potientially multi-lined script and turns it into a structured array.
 function ParseScript(script) {
-	if (!script) return [];
+	if (!script) {
+		return [];
+	}
+	
 	var lines = script.split('\n');
 	var cmdtree = [];
 
@@ -70,11 +128,11 @@ function ParseScript(script) {
 		var cmd = { line: i, code: [], src: lines[i], timeout: 5000 };  // setup cmd structure
 
 		var stmt = lines[i].match(/\w+|'[^']+'|"[^"]+"|\{\{(.*?)\}\}|\*|:/g);   // break the line into words, "quoted" or 'quoted' tokens, and {{tags}}
-		if (stmt)
+		if (stmt) {
 			if (stmt[0].charAt(0)!=='*') { 					//  We support bulleted lists of field/value pair.  If this is not one, then we process it differently.
 				for (j = 0; j < stmt.length; j++) {
 					var z = stmt[j].charAt(0);
-					if (z == '{' || z == '"' || z == "'" ) {
+					if (z === '{' || z === '"' || z === '\'' ) {
 						cmd.code.push(stmt[j]);
 					} else {
 						var candidate = stmt[j].toLowerCase();
@@ -104,7 +162,8 @@ function ParseScript(script) {
 							case 'on':
 							case 'in':
 							case 'into':
-								if ((cmd.code.length)  && (cmd.code[ cmd.code.length - 1 ] == 'in'))
+								console.log(cmd.code);
+								if ((cmd.code.length) && (cmd.code[ cmd.code.length - 1 ] === 'in'))
 									; // do nothing
 								else
 									cmd.code.push('in');
@@ -119,6 +178,7 @@ function ParseScript(script) {
 				cmd.code.push('in');
 				cmd.code.push(stmt[0].slice(1).trim());
 			}
+		}
 		cmdtree.push(cmd);
 	}
 	return cmdtree;
@@ -127,35 +187,42 @@ window.Herbie.StopScript = function() {
   stopScript = true;
 }
 
-function ExecuteScript( /* cmdtree, options, callback */) {
+function ExecuteScript() {
   var cmdtree = arguments[0], options = { line: 0, delay: 100, cmdtree:cmdtree } , callback, tag = [];
 
-	if (arguments.length == 2) { // only two arguments supplied
-		if (Object.prototype.toString.call(arguments[1]) == "[object Function]") {
+	if (arguments.length === 2) { // only two arguments supplied
+		if (Object.prototype.toString.call(arguments[1]) === '[object Function]') {
 			callback = arguments[1]; // if is a function, set as 'callback'
 		} else {
 			options = arguments[1]; // if not a function, set as 'options'
 		}
-	} else if (arguments.length == 3) { // three arguments supplied
-		if (arguments[1]) options = arguments[1];
+	} else if (arguments.length === 3) { // three arguments supplied
+		if (arguments[1]) {
+			options = arguments[1];
+		}
 		callback = arguments[2];
 	}
 
 	var i=options.line;
 	if (i>=cmdtree.length) {
-		if (callback) callback(true, options, "Finished.");
+		if (callback) {
+			callback(true, options, 'Finished.');
+		}
 		return;
 	}
 	if (stopScript) {
-		if (callback) callback(true, null, "Stopped.");
+		if (callback) {
+			callback(true, null, 'Stopped.');
+		}
 		return;
 	}
 
-	var inclause = $.inArray("in", cmdtree[i].code);
-	if (inclause!=-1) {
+	var inclause = $.inArray('in', cmdtree[i].code);
+	if (inclause !== -1) {
 		var tagname = cmdtree[i].code[inclause+1];
-		if (tagname.charAt(0)==='"'||tagname.charAt(0)==="'")
+		if (tagname.charAt(0)==='"'||tagname.charAt(0)==='\'') {
 			tagname = tagname.slice(1,-1);
+		}
 
 		tag = FindDesc( tagname );
 		if (!tag.length) {
@@ -164,22 +231,29 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 				return setTimeout(function () { ExecuteScript(cmdtree,options,callback); }, options.delay);
 			} else {
 				//  If we want to continue on error, we should callback(false), and then schedule the next call.
-				if (callback) callback(true, options, "Aborting. Cannot find tag named: '" + tagname + "'");
+				if (callback) {
+					callback(true, options, 'Aborting. Cannot find tag named: "' + tagname + '"');
+				}
 				return;
 			}
 		}
 	}
 
-	if (callback) callback(false, options);
+	if (callback) {
+		callback(false, options);
+	}
 
 	switch (cmdtree[i].code[0]) {
 		case 'press':
 			var seq = cmdtree[i].code[1];
-			if (seq.charAt(0)==='"'||seq.charAt(0)==="'")
+			if (seq.charAt(0)==='"'||seq.charAt(0)==='\'') {
 				seq = seq.slice(1,-1);
+			}
 
-			if (!tag.length) tag = $( document.activeElement );
-			tag.simulate("key-combo", {combo: seq });
+			if (!tag.length) {
+				tag = $( document.activeElement );
+			}
+			tag.simulate('key-combo', {combo: seq });
 			tag.next().focus();
 			return setTimeout(function () {
 					options.line++; // ok, setting the options to the next line here.
@@ -188,30 +262,37 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 
 		case 'type':
 			var seq = cmdtree[i].code[1];
-			if (seq.charAt(0)==='"'||seq.charAt(0)==="'")
+			if (seq.charAt(0)==='"'||seq.charAt(0)==='\'') {
 				seq = seq.slice(1,-1);
+			}
 
-			if (tag.length) tag.fadeOut(100)
-						.fadeIn(100)
-						.fadeOut(100)
-						.fadeIn(100)
-						.simulate("key-sequence", {sequence: seq, delay: options.delay, callback: 
-								function () { 
-									options.line++; // ok, setting the options to the next line here.
-									ExecuteScript(cmdtree,options,callback); 
-								}
-							});
+			if (tag.length) {
+				tag.fadeOut(100)
+					.fadeIn(100)
+					.fadeOut(100)
+					.fadeIn(100)
+					.simulate('key-sequence', {
+						sequence: seq,
+						delay: options.delay,
+						callback: function () {
+							options.line++; // ok, setting the options to the next line here.
+							ExecuteScript(cmdtree,options,callback);
+						}
+					});
+			}
 			return;
 		case 'click':
-			if (tag.length) tag.fadeOut(100)
-						.fadeIn(100)
-						.fadeOut(100)
-						.fadeIn(100)
-						.simulate("click");
+			if (tag.length) {
+				tag.fadeOut(100)
+					.fadeIn(100)
+					.fadeOut(100)
+					.fadeIn(100)
+					.simulate('click');
+			}
 			return setTimeout(function () {
-					options.line++; // ok, setting the options to the next line here.
-					ExecuteScript(cmdtree,options,callback);
-				}, options.delay);
+				options.line++; // ok, setting the options to the next line here.
+				ExecuteScript(cmdtree,options,callback);
+			}, options.delay);
 
 		default:
 			return setTimeout(function () {
@@ -224,7 +305,9 @@ function ExecuteScript( /* cmdtree, options, callback */) {
 
 window.Herbie.StartScript = function(opt, progress) {
 
-  if (progress) loaderCallback = progress;
+  if (progress) {
+	  loaderCallback = progress;
+  }
   stopScript = false;
 
   var script = '';
@@ -242,33 +325,50 @@ window.Herbie.StartScript = function(opt, progress) {
   	cmdtree = ParseScript(script);  	
   }
 
-  if (loaderCallback) loaderCallback( { event: "starting", script: script, options:opt } );
+  if (loaderCallback) {
+	  loaderCallback({
+		  event: 'starting',
+		  script: script,
+		  options: opt
+	  });
+  }
 
-  $("#herbie_run").text('Stop');
+  $('#herbie_run').text('Stop');
 
   if (cmdtree.length) {
-	  ExecuteScript(cmdtree, options, function (done, option, comment) { 
-	  	var out = $("#herbie_output");
+	  ExecuteScript(cmdtree, options, function (done, option, comment) {
+	  	var out = $('#herbie_output');
 	  	if (out.length) {
 	      if (done) {
-	          $("#herbie_run").text('Run');
+	          $('#herbie_run').text('Run');
 	      }
           if ((option) && (option.line < option.cmdtree.length)) {
-          	$("#herbie_line").val(option.line+1);
+          	$('#herbie_line').val(option.line+1);
           	out.append('Line: ' + (option.line+1) + ', Cmd:' + option.cmdtree[option.line].src + '\n');
           } else if (option) {
-          	$("#herbie_line").val('');          	
+          	$('#herbie_line').val('');
           }
-          if (comment) out.append('[' + comment + ']\n');
+          if (comment) {
+			  out.append('[' + comment + ']\n');
+          }
 	      out.animate({ scrollTop: out[0].scrollHeight}, 10);
 	    } else {
 	      console.log(done, option, comment);
 	    }
 		if (loaderCallback) {
-		  if (done)
-		 	loaderCallback( { event: "done", details: option, comment: comment } );
-		  else
-		  	loaderCallback( { event: "progress", details: option, comment: comment } );
+		  if (done) {
+		 	loaderCallback({
+				event: 'done',
+				details: option,
+				comment: comment
+			});
+		  } else {
+		  	loaderCallback({
+				event: 'progress',
+				details: option,
+				comment: comment
+			});
+		  }
 		}
 	  });
   }
@@ -276,50 +376,60 @@ window.Herbie.StartScript = function(opt, progress) {
 
 window.Herbie.Stop = function() {
   Herbie.StopScript();
-  $(".herbie_div").hide();
+  $('.herbie_div').hide();
 }
 
 window.Herbie.BuildUI = function(path, script, callback) {
-  if (callback) loaderCallback = callback;
+  if (callback) {
+	  loaderCallback = callback;
+  }
 
   // check to see if it's already in the page.  If it is, then no need to reload it.
-  if ($(".herbie_div").show().length) {
-	if (script) $("#herbie_script").text(script);
-	if (loaderCallback) loaderCallback( { event: "UIdone"} );
+  if ($('.herbie_div').show().length) {
+	if (script) {
+		$('#herbie_script').text(script);
+	}
+	if (loaderCallback) {
+		loaderCallback( { event: 'UIdone'} );
+	}
 	return;
   }
 
-  $('body').append("<div id='herbie'></div>");
-  $("#herbie").load(path+"herbie.html", function() {
+  $('body').append('<div id="herbie"></div>');
+  $('#herbie').load(path+'herbie.html', function() {
 	$(this).contents().unwrap();
-	$('#herbie_logo').attr("src",path+"../logos/herbie48.png");
-	if (script) $("#herbie_script").text(script);
+	$('#herbie_logo').attr('src',path+'../logos/herbie48.png');
+	if (script) {
+		$('#herbie_script').text(script);
+	}
 	$('.herbie_div').on('mousedown', function(e) {
 	  if (e.target.tagName === 'DIV') {
 	      $(this).addClass('herbie_draging').parents().on('mousemove', function(e) {
-	          $('.herbie_draging').css('right','auto').offset({
-	              top: e.pageY - $('.herbie_draging').outerHeight() / 2,
-	              left: e.pageX - $('.herbie_draging').outerWidth() / 2
-	          })
+	          var drag = $('.herbie_draging');
+	          drag.css('right','auto').offset({
+	              top: e.pageY - drag.outerHeight() / 2,
+	              left: e.pageX - drag.outerWidth() / 2
+	          });
 	      });
 	      e.preventDefault();
 	  }
 	}).on('mouseup', function() {
 	  var t = $('.herbie_draging');
 	  if (t.length) {
-	      if (t.offset().left < 0) 
+	      if (t.offset().left < 0) {
 	          t.offset({left:0});
-	      if (t.offset().top < 0) 
+		  }
+	      if (t.offset().top < 0) {
 	          t.offset({top:0});
-	      t.removeClass('herbie_draging');
-	      t.css('right','auto');
+		  }
+	      t.removeClass('herbie_draging').css('right','auto');
 	  }
 	});
 	$('.herbie_hide').on('click', function() {
 	  var ele = $(this),
 	      parent = ele.parent(),
 	      pparent = parent.parent();
-		  
+
 	  switch (ele.text()) {
 	      case 'Hide':
 	          pparent.find('div').hide();
@@ -337,25 +447,35 @@ window.Herbie.BuildUI = function(path, script, callback) {
 	          default:
 	  }
 	});
-	$("#herbie_add").click(function(){
-	  var txt = $('#herbie_script').val();
-	  if (!txt.match(/\n$/)) txt = txt + '\n'; // add a newline;
-	  txt += $("#herbie_command").val() + '\n';
-	  $('#herbie_script').val(txt);
-	  $("#herbie_command").val('').focus();
-  	  if (loaderCallback) loaderCallback( { event: "update", script: txt } );
+	$('#herbie_add').click(function(){
+	  var cmd = $('#herbie_command');
+	  var script = $('#herbie_script');
+	  var txt = script.val();
+	
+	  if (!txt.match(/\n$/)) {
+		  txt = txt + '\n'; // add a newline;
+	  }
+	  txt += cmd.val() + '\n';
+	  script.val(txt);
+	  cmd.val('').focus();
+  	  if (loaderCallback) {
+		  loaderCallback({
+			  event: 'update',
+			  script: txt
+		  });
+	  }
 	});
-	$("#herbie_parse").click(function(){
+	$('#herbie_parse').click(function(){
 	  var cmdtree = ParseScript($('#herbie_script').val());
 	  $('#herbie_output').text(JSON.stringify(cmdtree,null,2));
 	});
-	$("#herbie_run").click(function(){
+	$('#herbie_run').click(function(){
 	  switch ($(this).text()) {
 	  	case 'Run':
 		  $('#herbie_output').text('');
-		  Herbie.StartScript({ 
-		  	script: $('#herbie_script').val(), 
-		  	line: Math.max(Number($("#herbie_line").val())-1, 0)  
+		  Herbie.StartScript({
+		  	script: $('#herbie_script').val(),
+		  	line: Math.max(Number($('#herbie_line').val())-1, 0)
 		  });
 		  break;
 	  	case 'Stop':
@@ -363,17 +483,19 @@ window.Herbie.BuildUI = function(path, script, callback) {
 		  break;
 	  }
 	});
-	$("#herbie_inspect").click(function(){
+	$('#herbie_inspect').click(function(){
 	  $('.herbie_bar').hide();
 	  $('.herbie_script').hide();
 	  $('.herbie_output').show();
-	  document.RunInspector($("#herbie_output"), function() {
+	  document.RunInspector($('#herbie_output'), function() {
 	      $('.herbie_bar').show();
 	      $('.herbie_script').show();
 	  });
 	});
 
-	if (loaderCallback) loaderCallback( { event: "UIdone"} );
+	if (loaderCallback) {
+		loaderCallback( { event: 'UIdone'} );
+	}
   });
 }
 
